26ca8ec77188b59f3fa335b04b960e77e48f63d5
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-10-01 19:59:09 -0400

    started pseudocoding of js file

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Javascript for Mario Kart Randomizer App
+// Two seperate APIs will be added
+// https://github.com/samyvera/mk8_node_api
+// & TBD
+// Psedocode Below
+
+// define variables in accordance with the html file
+
+// if nothing is in localStorage, assign random character/car/course/ combination
+// Place Code Here
+
+// if there is a favorited character/car/course combination in localStorage, have that appear when user loads page
+// Place Code Here
+
