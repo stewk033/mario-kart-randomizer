dc0d3895d7f068a3c828bbdeaaded02e93cdabcf
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-10-01 20:03:30 -0400

    updated pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@
 // Two seperate APIs will be added
 // https://github.com/samyvera/mk8_node_api
 // & TBD
-// Psedocode Below
+// Pseudocode Below
 
 // define variables in accordance with the html file
 
-// if nothing is in localStorage, assign random character/car/course/ combination
+// if nothing is in localStorage, assign random character/car/course combination
 // Place Code Here
 
 // if there is a favorited character/car/course combination in localStorage, have that appear when user loads page
 // Place Code Here
 
+// Enable buttons from Bulma to be used:
+// Buttons for "generate combination" & a button for "start over/clear"
+// Place Code Here
+
+// Link APIs and Bulma library here - make sure to list BASE libraries first so that the additional ones will work
