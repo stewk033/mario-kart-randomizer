ea79085e648ab70b044201f5b96c1d7b1afa67a5
stewk033 <kevin.c.stewart04@gmail.com>
2021-10-04 14:01:14 -0400

    basic html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
-var myDate = new Date();
-
-console.log(myDate);
+fetch('https://mario-kart-tour-api.herokuapp.com/api/v1/drivers/name?q=mario', {
+	method: "GET",
+	headers: {"Content-type": "application/json;charset=UTF-8", "authorization": "Bearer YOUR_BEARER"}
+})
+ .then(response => response.json()) 
+ .then(json => console.log(json));
